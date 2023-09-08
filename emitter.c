c0b92ee831d3ee723f62d5d53fa8626d7a3208e6
Seungwoo Choe <seungwoochoes@gmail.com>
2023-09-08 10:33:41 +0200

    Modify the code for lab-2 part B

--- emitter.c
+++ emitter.c
@@ -5,7 +5,7 @@
 void emit (int token_type, int token_value)  /*  generates output  */
 {
     switch(token_type) {
-    case '+' : case '-' : case '*' : case '/':
+    case '+' : case '-' : case '*' : case '/' : case '=':
         printf("%c\n", token_type); break;
     case DIV:
         printf("DIV\n"); break; 
