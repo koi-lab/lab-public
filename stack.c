7530f61337700678d286901938c12cd2b5a7ac97
Seungwoo Choe <seungwoochoes@gmail.com>
2023-09-12 09:12:16 +0200

    Modify the way printStack prints out things

--- stack.c
+++ stack.c
@@ -30,13 +30,14 @@ int peek(Stack* stack) {
 
 void printStack(Stack* stack) {
     if (stack->top < 0) {
-        printf("Stack is empty.\n");
+        printf("  Stack is empty.\n");
         return;
     }
 
-    printf("Stack elements from top to bottom:\n");
+    printf("  Stack elements: ");
     for (int i = stack->top; i >= 0; i--) {
-        printf("%d\n", stack->data[i]);
+        printf("%d", stack->data[i]);
     }
+    printf("\n");
 }
 
